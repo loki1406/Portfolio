73a4b70bab43ed4ddc20af8a40857a7666a5e139
Lokesh Kuna <lokeshkuna1406@gmail.com>
2018-06-12 21:36:15 +0530

    Day 2 half done

--- javascript.js
+++ javascript.js
@@ -8,19 +8,24 @@ $('#onepage').fullpage({
 });
 
 
-  $(document).on('mousewheel keyup keydown', function(){
+  $(document).on('click mousewheel keyup keydown', function(){
     var x = $('#navli').attr('class');
-  
-      if (x == 'active') {
-        $('#navbar').css('background-color', '#020202');
-        $('li').css('color','#fff');
-        $('#navli a').css('color', '#286DA8');
-      }
-      else {
-        $('#navbar').css('background-color', 'white');
-        $('li').css('color','#000000');
-        $('#navli a').css('color', '#000000');
-      }
+    if (x == 'active') {
+      $('#navbar').css('background-color', '#020202');
+      $('#navul li').css('color','#fff');
+      $('#navli a').css('color', '#438496');
+      $('nav ul').css('margin-top', '5.1vh');
+      $('nav ul').css('margin-bottom', '5.1vh');
+    }
+    else {
+      $('#navbar').css('background-color', 'white');
+      $('#navul li').css('color','#707070');
+      $('#navli a').css('color', '#707070');
+      $('nav ul').css('margin-top', '3.2vh');
+      $('nav ul').css('margin-bottom', '3.2vh');
+    }
+
+
 
 
 
